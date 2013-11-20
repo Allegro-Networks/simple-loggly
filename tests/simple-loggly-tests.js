a73bb1e2195ea8162ec44f9f6be5b1ee2226a9ae
Sausage <wardrox@gmail.com>
2013-11-20 10:28:32 +0000

    added optional tag parameters

--- tests/simple-loggly-tests.js
+++ tests/simple-loggly-tests.js
@@ -4,7 +4,7 @@ var assert = require('assert'),
 
 test('When I Log an error, Then the correct uri is posted to', function() {
 	var logglyKey = 'loggly-key',
-		expectedPostUri = 'https://logs-01.loggly.com/inputs/' + logglyKey + '/tag/http-client/',
+		expectedPostUri = 'https://logs-01.loggly.com/inputs/' + logglyKey + '/',
 		postedUri;
 	var FakeHttpRequest = {
 		post: function(url, data, callback, options) {
@@ -34,4 +34,36 @@ test('When I Log an error, Then the correct data is posted', function() {
 	assert.deepEqual(postedData, data);
 });
 
+test('When I Log an error with a single tag, Then the correct uri is posted to', function() {
+	var logglyKey = 'loggly-key',
+		expectedPostUri = 'https://logs-01.loggly.com/inputs/' + logglyKey + '/tag/testtag1',
+		logglyTag = ["testtag1"],
+		postedUri;
 
+	var FakeHttpRequest = {
+		post: function(url, data, callback, options) {
+			postedUri = url;
+			callback();
+		}
+	};
+	var simpleLoggly = new SimpleLoggly(FakeHttpRequest, logglyKey, logglyTag);
+	simpleLoggly.log("");
+	assert.equal(postedUri, expectedPostUri);
+});
+
+test('When I Log an error with multiple tags, Then the correct uri is posted to', function() {
+	var logglyKey = 'loggly-key',
+		expectedPostUri = 'https://logs-01.loggly.com/inputs/' + logglyKey + '/tag/testtag1,taggingAgain,onceMoreForLuck',
+		logglyTag = ["testtag1", "taggingAgain", "onceMoreForLuck"],
+		postedUri;
+
+	var FakeHttpRequest = {
+		post: function(url, data, callback, options) {
+			postedUri = url;
+			callback();
+		}
+	};
+	var simpleLoggly = new SimpleLoggly(FakeHttpRequest, logglyKey, logglyTag);
+	simpleLoggly.log("");
+	assert.equal(postedUri, expectedPostUri);
+});
